cfe0b9fdfd6c44c1ba795277414f74d554d0c986
Maria <mribaltaalbado@gmail.com>
2024-05-11 11:43:30 +0200

    Add Mixtral code

--- src/common.py
+++ src/common.py
@@ -1,15 +1,59 @@
+import os
+import pandas as pd
+import json
+import re
+
 
 def generate_mapper(row):
-  return {
+    return {
         "[ORIGINAL LANGUAGE]": row["l1"],
         "[TRANSLATED LANGUAGE]": row["l2"],
         "[ORIGINAL TEXT]": row["src"],
         "[TRANSLATED TEXT]": row["mt"],
     }
 
+
 def get_prompt(prompt_template: str, mapper: dict):
     prompt = prompt_template
     for tag, value in mapper.items():
         prompt = prompt.replace(tag, value)
-    
+
     return prompt
+
+
+def recover_all_files(path: str):
+    files_content = []
+
+    for filename in os.listdir(path):
+        with open(f"{path}/{filename}") as infile:
+            file_content = json.load(infile)
+            files_content += file_content
+    return pd.DataFrame(files_content).sort_values(by="id").reset_index(drop=True)
+
+
+def compute_correlation(
+    df: pd.DataFrame,
+    ground_truth: str = "raw",
+    predicted: str = "gemma_score",
+    method: str = "pearson",
+    decimals=3,
+):
+    correlations = {
+        "overall": round(df[ground_truth].corr(df[predicted], method=method), decimals)
+    }
+
+    for language_pair in df["lp"].unique():
+        sub_df = df[df.lp == language_pair]
+        correlations[language_pair] = round(
+            sub_df[ground_truth].corr(sub_df[predicted], method=method), decimals
+        )
+
+    return correlations
+
+def get_score(response: str):
+    pattern = r"\d+(?:\.\d+)?"
+    
+    match = re.search(pattern, response)
+    score = float(match.group(0)) if match else None
+
+    return score
--- src/fun.py
+++ src/fun.py
@@ -5,12 +5,14 @@ import re
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 
+
 def get_prompt(prompt_template: str, mapper: dict):
     prompt = prompt_template
     for tag, value in mapper.items():
         prompt = prompt.replace(tag, value)
-    
+
     return prompt
+
 
 def send_message_mistral(
     client: MistralClient, message: str, mistral_model="open-mistral-7b"
@@ -32,11 +34,15 @@ def send_message_mistral(
     )
     return chat_response.choices[0].message.content
 
-    
-def ask_mistral_for_evaluation(client, prompt_template, mapper, mistral_model="open-mistral-7b"):
+
+def ask_mistral_for_evaluation(
+    client, prompt_template, mapper, mistral_model="open-mistral-7b"
+):
     message = get_prompt(prompt_template, mapper)
-    response = send_message_mistral(client=client, message=message, mistral_model=mistral_model)
-    
+    response = send_message_mistral(
+        client=client, message=message, mistral_model=mistral_model
+    )
+
     match = re.match(r"\d+", response)
     score = int(match) if match else None
 
